707980db5a44e336276453c86bcf8f520c5407df
admin <root@localhost>
2023-01-19 20:55:20 +0000

    ff

--- custom_functions/excel_to_json.py
+++ custom_functions/excel_to_json.py
@@ -27,8 +27,6 @@ def excel_to_json(vault_id=None, container_id=None, **kwargs):
     json_str = json.load(j)
     phantom.debug(type(json_str))
     
-    y = dict((k.lower(), v) for k, v in json_str.iteritems())
-    
 
     # Return a JSON-serializable object
     assert json.dumps(outputs)  # Will raise an exception if the :outputs: object is not JSON-serializable
