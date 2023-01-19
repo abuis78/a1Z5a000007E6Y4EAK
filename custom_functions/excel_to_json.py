8051ec27c8ab49a5dbece9a49fa4e79a122c4933
admin <root@localhost>
2023-01-19 14:17:08 +0000

    ff

--- custom_functions/excel_to_json.py
+++ custom_functions/excel_to_json.py
@@ -17,6 +17,8 @@ def excel_to_json(vault_id=None, container_id=None, **kwargs):
     success, message, info = phantom.vault_info(vault_id=vault_id, container_id=container_id)
     phantom.debug(info[0]["path"])
     phantom.debug(info[0]["name"])
+    file = info[0]["path"] + "/" + info[0]["name"]
+    phantom.debug(file)
     
     
     # Return a JSON-serializable object
