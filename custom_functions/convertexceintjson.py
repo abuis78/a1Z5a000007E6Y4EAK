ccd6e177efacd3a2def9a818b1dd846a3cb753b5
admin <root@localhost>
2023-01-19 13:08:44 +0000

    ddd

--- custom_functions/convertexceintjson.py
+++ custom_functions/convertexceintjson.py
@@ -9,12 +9,13 @@ def convertexceintjson(vault_id=None, **kwargs):
     ############################ Custom Code Goes Below This Line #################################
     import json
     import phantom.rules as phantom
+    import excel2json
     
     outputs = {}
     
     # Write your custom code here...
-    phantom.vault_info(vault_id=vault_id, file_name=None, container_id=None, trace=False)
-
+    success, message, info = phantom.vault_info(vault_id=vault_id, file_name=None, container_id=None, trace=False)
+    phantom.debug(info)
     
     # Return a JSON-serializable object
     assert json.dumps(outputs)  # Will raise an exception if the :outputs: object is not JSON-serializable
