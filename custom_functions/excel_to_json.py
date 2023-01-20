5cf2c71b71910bd0ee63145ebdc32e2f65487896
admin <root@localhost>
2023-01-20 07:54:21 +0000

    ff

--- custom_functions/excel_to_json.py
+++ custom_functions/excel_to_json.py
@@ -22,7 +22,9 @@ def excel_to_json(vault_id=None, container_id=None, **kwargs):
     file = info[0]["path"]
     phantom.debug(file)
     
-    excel_data_df = pandas.read_excel(file, sheet_name='Sheet1', names=["pool","virtualmachine", "user"])
+    #excel_data_df = pandas.read_excel(file, sheet_name='Sheet1', names=["pool","virtualmachine", "user"])
+    excel_data_df = pandas.read_excel(file, sheet_name='Sheet1')
+    execl_data_df = excel_data_df(columns = {"Pool":"pool", "User": "user", "Virtual Machine": "virtualmachine"} )
     j_dict = excel_data_df.to_json(orient='records')
     #j_dict = json.loads(j_dict)
     phantom.debug(j_dict)
