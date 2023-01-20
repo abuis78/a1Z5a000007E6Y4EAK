4edc99f9c3284905f00700eb37406adab9140925
admin <root@localhost>
2023-01-20 07:23:39 +0000

    ff

--- custom_functions/excel_to_json.py
+++ custom_functions/excel_to_json.py
@@ -5,7 +5,7 @@ def excel_to_json(vault_id=None, container_id=None, **kwargs):
         container_id
     
     Returns a JSON-serializable object that implements the configured data paths:
-        
+        j_dict
     """
     ############################ Custom Code Goes Below This Line #################################
     import json
@@ -28,7 +28,7 @@ def excel_to_json(vault_id=None, container_id=None, **kwargs):
     phantom.debug(j_dict)
     phantom.debug(type(j_dict))
     
-
+    outputs["outputs"] = j_dict
     # Return a JSON-serializable object
     assert json.dumps(outputs)  # Will raise an exception if the :outputs: object is not JSON-serializable
     return outputs
