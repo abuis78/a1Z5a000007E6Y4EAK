fb385c5938bd96d3a04cf5c8e49fe339e5022c91
admin <root@localhost>
2023-01-20 08:54:18 +0000

    ff

--- test.py
+++ test.py
@@ -123,7 +123,7 @@ def delete_data_1(action=None, success=None, container=None, results=None, handl
     ## Custom Code End
     ################################################################################
 
-    phantom.act("delete data", parameters=parameters, name="delete_data_1", assets=["splunk_rest"], callback=filter_1)
+    phantom.act("delete data", parameters=parameters, name="delete_data_1", assets=["splunk_rest"])
 
     return
 
